0f39129c8272c8601b55185bdab3f396cb5a1eb9
GLXB <ustynow.gleb@gmail.com>
2026-09-13 22:35:01 +0300

    feat: Bitmasks, config init, etc.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,26 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QDir>
+
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
+    , config(nullptr)
+    , running(false)
 {
     ui->setupUi(this);
+
+    ui->inputPathInput->setPlaceholderText(QDir::currentPath());
+    ui->outputPathInput->setPlaceholderText(QDir::currentPath());
+
+    ui->duplicatePolicyGroup->setId(
+        ui->renameButton,
+        Config::DuplicatesPolicy::kRename);
+    ui->duplicatePolicyGroup->setId(
+        ui->rewriteButton,
+        Config::DuplicatesPolicy::kRewrte);
 }
 
 MainWindow::~MainWindow()
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QRegularExpression>
+#include <QString>
+
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -18,6 +21,21 @@ public:
     ~MainWindow() override;
 
 private:
+    struct Config {
+        enum DuplicatesPolicy {kRewrte, kRename};
+
+        QString input_path;
+        QString output_path;
+        QString operator_;
+        QRegularExpression file_regex;
+        uint64_t bitmask;
+        DuplicatesPolicy policy;
+        bool remove_processed;
+    };
+
     Ui::MainWindow *ui;
+    Config *config;
+    bool running;
 };
+
 #endif // MAINWINDOW_H
